c5f0e7fe38853279d0a8949b74922018664bab2a
William LHL <william.lionhart@lighthouselabs.com>
2023-03-12 21:55:52 +0000

    restored original state of actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -1,17 +1,3 @@
-def fizzbuzz(test)
-  myarray = []
-  if test % 3 == 0 && test % 5 == 0
-    "FizzBuzz" && myarray.push("FizzBuzz")
-  elsif test % 3 == 0
-    "Fizz" && myarray.push("Fizz")
-  elsif test % 5 == 0
-    "Buzz" && myarray.push("Buzz")
-  else test % 3 > 0 || test % 5 > 0
-    "#{test}" && myarray.push("#{test}")
-  end
-  myarray.to_s
-end
-
-get '/' do 
-  fizzbuzz(15)
+get '/' do
+  File.read(File.join('app/views', 'index.html'))
 end
