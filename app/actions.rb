fb326034ad3033c60225a7d0c40a86dc12c08a7a
William LHL <william.lionhart@lighthouselabs.com>
2023-03-12 10:53:27 +0000

    add Ruby code

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,18 @@
-get '/' do
-    File.read(File.join('app/views', 'index.html'))
+def fizzbuzz(test)
+  if test % 15 == 0
+    "FizzBuzz"
+  elsif test % 3 == 0
+    "Fizz"
+  elsif test % 5 == 0
+    "Buzz"
+  elsif test % 3 > 0
+    "#{test}"
+  elsif test % 5 > 0
+    "#{test}"
   end
+end
+
+get '/' do
+  test = 9
+  fizzbuzz(test)
+end
