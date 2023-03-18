c91a01a95d7a4c3d059390eef4d83fecfda4cd4c
William LHL <william.lionhart@lighthouselabs.com>
2023-03-18 19:13:17 +0000

    added more ruby code

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,53 @@
+def humanized_time_ago(minute_num)
+  if minute_num >= 60
+    "#{minute_num / 60} hours ago"
+  else
+    "#{minute_num} minutes ago"
+  end
+end
+
 get '/' do
-  File.read(File.join('app/views', 'index.erb'))
+  @finstagram_post_shark = {
+    username: "sharky_j",
+    avatar_url: "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg",
+    photo_url: "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+  }
+
+  @finstagram_post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "https://live.staticflickr.com/65535/52358421348_f34c7996b1.jpg",
+    photo_url: "https://live.staticflickr.com/65535/52357237337_1cc718f6a7_4k.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
+  }
+
+  @finstagram_post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "https://live.staticflickr.com/65535/52358415933_0a0e6bc35f_3k.jpg",
+    photo_url: "https://live.staticflickr.com/65535/52358494794_f88b160d15_4k.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+
+  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
   erb(:index)
 end
 
