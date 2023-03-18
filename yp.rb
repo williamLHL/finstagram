f2273d7997c61dacca705803c4c1c5c0fe9d4079
William LHL <william.lionhart@lighthouselabs.com>
2023-03-18 20:49:46 +0000

    created a hash for numkey assignment

--- yp.rb
+++ yp.rb
@@ -1,8 +1,19 @@
 puts "Without spaces, type in a 10-character string below:"
 input = gets
 
+numkey = {
+    2 => ["A","B","C"],
+    3 => ["D","E","F"],
+    4 => ["G","H","I"],
+    5 => ["J","K","L"],
+    6 => ["M","N","O"],
+    7 => ["P","Q","R", "S"],
+    8 => ["T","U","V"],
+    9 => ["W","X","Y","Z"]
+}
+
 if input.length > 10
     then puts "You have exceeded the character limit. Try again."
 elsif input.length <=10 
-    puts "Is this what you typed? #{input}"
+    then puts "Is this what you typed? #{input}"
 end
