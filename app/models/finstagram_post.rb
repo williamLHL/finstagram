efe757ad1073d9e3db433c1d03bfbcfbadbae18b
William LHL <william.lionhart@lighthouselabs.com>
2023-03-25 19:57:23 +0000

    added associations to ruby files

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,3 +1,5 @@
 class FinstagramPost < ActiveRecord::Base
 
+    belongs_to :user
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
-class Like ActiveRecord::Base
+class Like < ActiveRecord::Base\
+
+    belongs_to :user
+    belongs_to :finstagram_post
+
 end
