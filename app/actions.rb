3f7ed363a9ad28c3c3a49ce3d859408ab10eab4e
William LHL <william.lionhart@lighthouselabs.com>
2023-04-01 21:57:26 +0000

    added login form and action; added session hash; added snippet to keep track of current user; added user redirections

--- app/actions.rb
+++ app/actions.rb
@@ -1,20 +1,22 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
 
-get '/signup' do     # if a user navigates to the path "/signup",
-  @user = User.new   # setup empty @user object
-  erb(:signup)       # render "app/views/signup.erb"
-end
-
-get "/login" do
-  erb(:login)
-end
-
 get '/experindex' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:experindex) 
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
 end
 
 post '/signup' do
@@ -29,8 +31,32 @@ post '/signup' do
 
   # if user validations pass and user is saved
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+get "/login" do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  @user = User.find_by(username: username)
+
+  if @user && @user.password == password
+    session[:user_id] = @user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
