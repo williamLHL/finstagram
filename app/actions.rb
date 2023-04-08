a951f44a11a88842ddd1141d00182a1f83e0b90a
William LHL <william.lionhart@lighthouselabs.com>
2023-04-08 21:31:17 +0000

    implement the ability to add comments with necessary view and actions; added validations for comment records, implemented simple responsive CSS

--- app/actions.rb
+++ app/actions.rb
@@ -87,3 +87,18 @@ get '/finstagram_posts/:id' do
   # render app/views/finstagram_posts/show.erb
   erb(:"finstagram_posts/show")               
 end
+
+post '/comments' do
+  # point values from params to variables
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  # instantiate a comment with those values & assign the comment to the `current_user`
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  # save the comment
+  comment.save
+
+  # `redirect` back to wherever we came from
+  redirect(back)
+end
