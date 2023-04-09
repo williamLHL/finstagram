8fe974b140a7315dd376adf462798b923fe2e468
William LHL <william.lionhart@lighthouselabs.com>
2023-04-09 19:35:06 +0000

    implemented the ability add and remove likes; created necessary view and actions; added validations for Like records; implemented simple responsive CSS

--- app/actions.rb
+++ app/actions.rb
@@ -102,3 +102,18 @@ post '/comments' do
   # `redirect` back to wherever we came from
   redirect(back)
 end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
